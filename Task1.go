9b14859f4a4bc823c4b509f2399ee19e3f7840df
Роман Бабкин <spothyjohn@gmail.com>
2025-09-13 12:45:32 +0300

    Create Task1.go

---
+++ Task1.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func printNumbers(wg *sync.WaitGroup) {
+	defer wg.Done()
+	for i := 1; i <= 5; i++ {
+		fmt.Println(i)
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func main() {
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go printNumbers(&wg)
+	wg.Wait()
+}
